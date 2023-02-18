0867894b04d83f1b39260cfbcfbfa0d0fd2c2ac1
Trevor Atlas <me@trevoratlas.com>
2023-02-17 20:09:55 -0500

    whoops

--- utils/encoding.ts
+++
@@ -1,7 +0,0 @@
-export function toBase64(str: string): string {
-  return window.btoa(encodeURIComponent(str));
-}
-
-export function fromBase64(str: string): string {
-  return decodeURIComponent(window.atob(str));
-}
--- utils/preload-image.ts
+++
@@ -1,7 +0,0 @@
-const preloadImage = (src: string): Promise<HTMLImageElement> =>
-  new Promise((resolve, reject) => {
-    const img = new Image();
-    img.onload = () => resolve(img);
-    img.onerror = img.onabort = () => reject(src);
-    img.src = src;
-  });
