02ef36a93062861aaf75233519bee72ec4c883eb
Trevor Atlas <me@trevoratlas.com>
2023-03-04 13:07:43 -0500

    update barrel export

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,10 @@
-import * as Common from './types/common';
-import * as Dom from './utils/dom';
-import * as Algo from './utils/algo';
-import * as Arrays from './utils/arrays';
-import * as Chrono from './utils/chrono';
-import * as Utils from './utils/common';
-import * as Encoding from './utils/encoding';
-import * as Functional from './utils/functional';
-import * as Storage from './utils/local-storage';
-import * as Numeric from './utils/numeric';
-
-export {
-  Common,
-  Dom,
-  Algo,
-  Arrays,
-  Chrono,
-  Utils,
-  Encoding,
-  Functional,
-  Storage,
-  Numeric,
-};
+export * from './types/common';
+export * from './utils/dom';
+export * from './utils/algo';
+export * from './utils/arrays';
+export * from './utils/chrono';
+export * from './utils/common';
+export * from './utils/encoding';
+export * from './utils/functional';
+export * from './utils/local-storage';
+export * from './utils/numeric';
